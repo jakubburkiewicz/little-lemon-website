188f42c6f4205f03f11e8e81c964fe63440d6d84
Jakub Burkiewicz <jakub.burkiewicz@conx.eu>
2024-05-17 17:17:44 +0200

    Switch basket and hamburger icons from images to components

--- src/components/BasketToggle.jsx
+++ src/components/BasketToggle.jsx
@@ -1,14 +1,12 @@
+import BasketIcon from "../icons/BasketIcon"
+
 const BasketToggle = props => (
     <button
         type="button"
         className={ props.className }
         onClick={ props.onClick }
     >
-        <img
-            className="basketToggle__icon"
-            src="/Basket.svg"
-            alt="basket"
-        />
+        <BasketIcon />
     </button>
 )
 
--- src/components/HamburgerToggle.jsx
+++ src/components/HamburgerToggle.jsx
@@ -1,14 +1,12 @@
+import HambugerIcon from "../icons/HamburgerIcon"
+
 const HamburgerToggle = props => (
     <button
         type="button"
         className={ props.className }
         onClick={ props.onClick }
     >
-        <img
-            className="hamburgerToggle__icon"
-            src="/hamburger.svg"
-            alt="menu"
-        />
+        <HambugerIcon />
     </button>
 )
 
