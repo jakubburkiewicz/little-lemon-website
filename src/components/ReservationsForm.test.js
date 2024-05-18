ca3af67d6fd1407b319866c094ac2624745b4329
Jakub Burkiewicz <jakub.burkiewicz@conx.eu>
2024-05-18 20:11:22 +0200

    Add tests for form validation

--- src/components/ReservationsForm.test.js
+++ src/components/ReservationsForm.test.js
@@ -1,8 +1,9 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import ReservationsForm from './ReservationsForm';
 
 const availableTimes = [ '17:00', '18:00', '19:00' ];
 const mockOnDateChange = jest.fn();
+const mockOnFormSubmit = jest.fn(event => event.preventDefault());
 
 describe( 'ReservationsForm', () => {
   test('renders the label for the date input field', () => {
@@ -15,4 +16,48 @@ describe( 'ReservationsForm', () => {
 
     expect( screen.getByLabelText('Choose date') ).toBeInTheDocument();
   } );
+
+  test( 'validates the form', () => {
+    render(
+      <ReservationsForm
+        availableTimes={ availableTimes }
+        onDateChange={ mockOnDateChange }
+        onFormSubmit={ mockOnFormSubmit }
+      />
+    );
+
+    const dateInput = screen.getByLabelText( 'Choose date' );
+    const timeInput = screen.getByLabelText( 'Choose time' );
+    const guestsInput = screen.getByLabelText( 'Number of guests' );
+    const occasionInput = screen.getByLabelText( 'Occasion' );
+    const submitButton = screen.getByRole( 'button', { name: 'Make Your reservation' } );
+
+    // Setting initial valid values
+    fireEvent.change( dateInput, { target: { value: '2022-12-31' } } );
+    fireEvent.change( timeInput, { target: { value: '17:00' } } );
+    fireEvent.change( occasionInput, { target: { value: 'Birthday' } } );
+
+    // Test invalid guests value (0)
+    fireEvent.change( guestsInput, { target: { value: 0 } } );
+    fireEvent.click( submitButton );
+    expect( mockOnFormSubmit ).not.toHaveBeenCalled();
+
+    // Test valid guests value (1)
+    fireEvent.change( guestsInput, { target: { value: 1 } } );
+    fireEvent.click( submitButton );
+    expect( mockOnFormSubmit ).toHaveBeenCalledTimes( 1 );
+
+    // Reset mock function
+    mockOnFormSubmit.mockReset();
+
+    // Test invalid guests value (11)
+    fireEvent.change( guestsInput, { target: { value: 11 } } );
+    fireEvent.click( submitButton );
+    expect( mockOnFormSubmit ).not.toHaveBeenCalled();
+
+    // Test valid guests value (10)
+    fireEvent.change( guestsInput, { target: { value: 10 } } );
+    fireEvent.click( submitButton );
+    expect( mockOnFormSubmit ).toHaveBeenCalledTimes( 1 );
+  });
 } );
